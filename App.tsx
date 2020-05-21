44467708d429fb624fb0452b8fc60e27c28788fa
M. Arif Sefrianto <arifsefrianto@gmail.com>
2020-05-21 21:53:14 +0700

    update;

--- App.tsx
+++ App.tsx
@@ -1,18 +1,82 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Alert,
+  Platform,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
-  let x = 1;
   console.log('App exe');
 
   return (
-    <View style={styles.container}>
-      <Text>Abc</Text>
-      <Text>Abc</Text>
-      <Text>Abc</Text>
-      <Text>Abc</Text>
-      <Text>Abssc</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2}>
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc AAbcAbcAbcAbcAbcAbc
+        AAbcAbcAbcAbcAbcAbcs
+      </Text>
+      <TouchableWithoutFeedback
+        onPress={() =>
+          Alert.alert('Title', 'Message', [
+            {
+              text: 'yes',
+              onPress: () => {
+                console.log('Yes');
+              },
+            },
+            {
+              text: 'no',
+              onPress: () => {
+                console.log('No');
+              },
+            },
+          ])
+        }>
+        <Image
+          fadeDuration={300}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableWithoutFeedback>
+      <TouchableOpacity
+        onPress={() =>
+          Alert.prompt('Title', 'Message', (text) => console.log(text))
+        }>
+        <Image
+          fadeDuration={300}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableHighlight
+        onPress={() => console.log('Image TouchableHighlight')}>
+        <Image
+          fadeDuration={300}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
@@ -20,7 +84,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
